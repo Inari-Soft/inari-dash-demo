1d2e3e6e81ad11fedda7d83c9ea7d8293d40f665
andreas hefti <anhefti@yahoo.de>
2015-12-28 22:49:48 +0100

    Animation refactoring and new AnimationResolver component

--- src/main/java/com/inari/dash/game/cave/unit/UnitController.java
+++ src/main/java/com/inari/dash/game/cave/unit/UnitController.java
@@ -2,7 +2,6 @@ package com.inari.dash.game.cave.unit;
 
 import com.inari.dash.game.cave.CaveSystem;
 import com.inari.firefly.audio.AudioSystem;
-import com.inari.firefly.component.attr.AttributeKey;
 import com.inari.firefly.entity.EntityController;
 import com.inari.firefly.system.FFContext;
 
@@ -16,11 +15,6 @@ public abstract class UnitController extends EntityController {
         super( id, context );
         caveService = context.getSystem( CaveSystem.SYSTEM_KEY );
         soundSystem = context.getSystem( AudioSystem.SYSTEM_KEY ); 
-    }
-
-    @Override
-    public AttributeKey<?>[] getControlledAttribute() {
-        return null;
     }
 
 }
